0152e30e432451ab65bcb34b126fe03c86caa863
Paulius Gerve <paulius.gerve@oxylabs.io>
2024-12-13 11:14:33 +0200

    update string_ names

--- gyvatukas/utils/lithuania.py
+++ gyvatukas/utils/lithuania.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 import datetime
 from typing import Literal, Optional
 
-from gyvatukas.utils.string_ import remove_except
+from gyvatukas.utils.string_ import str_remove_except
 
 
 @dataclass
@@ -125,7 +125,7 @@ def validate_lt_tel_nr(tel_nr: str, format_370: bool = True) -> tuple[bool, str]
     is_valid = False
 
     # Remove all symbols except + and 0-9.
-    clean_tel_nr = remove_except(
+    clean_tel_nr = str_remove_except(
         tel_nr, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+"]
     )
 
