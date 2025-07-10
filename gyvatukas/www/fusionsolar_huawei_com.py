71febd1ce54cd63a7bb49513f44fa4b9caea9929
pauliusbaulius <dev@paulius.xyz>
2025-07-10 16:33:56 +0300

    todo

---
+++ gyvatukas/www/fusionsolar_huawei_com.py
@@ -0,0 +1,5 @@
+# todo: https://eu5.fusionsolar.huawei.com simple wrapper to extract stats, aka read-only wrapper.
+#  Available libraries:
+#  https://github.com/EnergieID/FusionSolar
+#  https://github.com/jgriss/FusionSolarPy
+#  https://github.com/guillaumeblanc/pyhfs
